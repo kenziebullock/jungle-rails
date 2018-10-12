64cfe311141047a0f22bb03600e26dec1f113517
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-10-12 19:18:58 +0000

    added simple admin auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    
     user_name:            'no.reply.jungle',
     password:             'junglejungle',
     authentication:       'plain',
